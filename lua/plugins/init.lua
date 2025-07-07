8165079b640b24d04228dbbee1dffc1ec3ffd36c
songliyu <songliyu@tencent.com>
2025-07-07 10:28:22 +0800

    Update markdown-preview's settings.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -998,4 +998,11 @@ return {
       },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    init = function()
+      vim.g.mkdp_auto_close = 0
+      vim.g.mkdp_combine_preview = 1
+    end,
+  },
 }
