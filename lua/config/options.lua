54214e8341578617e1eb338caa02dcb84c7b2a52
ysl2 <songliyu97@gmail.com>
2025-07-26 08:58:56 +0800

    Add typescript language support.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -48,7 +48,7 @@ vim.opt.smartindent = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
+  pattern = { "lua", "typescript", "javascript", "json", "jsonc" },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
