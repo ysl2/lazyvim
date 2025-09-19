d59f8f1409a3978339ef63f6919dc91b12e2a1c4
ysl2 <songliyu97@gmail.com>
2025-09-19 11:10:10 +0800

    Add comment for render-markdown.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -936,7 +936,10 @@ return {
     },
   },
   {
+    -- NOTE: This plugin is imported by LazyVim.
+    -- So if you want to disable it, you need to set `enabled = false` here. Not just comment it out.
     "MeanderingProgrammer/render-markdown.nvim",
+    -- NOTE: Disable it for now since it messes up with markdown, and I don't use Avante now.
     enabled = false,
     -- ft = { "markdown", "norg", "rmd", "org", "Avante" },
     ft = "Avante",
