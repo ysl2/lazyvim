5a67bf6630ed46f1d43cfbcff6caa1338f03d5f5
ysl2 <songliyu97@gmail.com>
2025-10-11 20:05:38 +0800

    Pin lazyvim version to v14.15.1, with neovim version v0.11.1

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", version = "14.15.1", pin = true },
     -- import/override with your plugins
     { import = "plugins" },
   },
