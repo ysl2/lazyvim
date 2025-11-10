c6500aaf698cbe9ea5090281bbaaeafb88ba6788
ysl2 <songliyu97@gmail.com>
2025-11-10 12:10:27 +0800

    Enable toggle terminal in insert and vision mode.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -290,7 +290,7 @@ return {
           end
           Snacks.terminal(cmd, { cwd = LazyVim.root() })
         end,
-        mode = { "n", "t" },
+        mode = { "n", "i", "v", "t" },
         desc = "Tmux (Root Dir)",
       },
       {
