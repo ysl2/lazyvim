a7ecbf5bb5df83bdbebb33d903a3259aec78fb80
ysl2 <songliyu97@gmail.com>
2026-04-24 14:55:47 +0800

    add autocmd custom_checktime

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,6 +8,25 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Reload buffers when files are changed by external tools while Nvim stays focused.
+vim.api.nvim_del_augroup_by_name("lazyvim_checktime")
+vim.api.nvim_create_autocmd({
+  "FocusGained",
+  "TermClose",
+  "TermLeave",
+  "BufEnter",
+  -- "CursorHold",
+  -- "CursorHoldI",
+  "ShellCmdPost",
+}, {
+  group = vim.api.nvim_create_augroup("custom_checktime", { clear = true }),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
 -- Auto delete [No Name] buffers.
 vim.api.nvim_create_autocmd("BufLeave", {
   callback = function(args)
